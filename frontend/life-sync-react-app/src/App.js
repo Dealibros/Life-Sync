01881f18a448b02572054085b03f2f274c071c1b
Dealibros <dealibros88@gmail.com>
2022-12-13 17:34:56 +0100

    connection with backend and frontend done

--- frontend/life-sync-react-app/src/App.js
+++ frontend/life-sync-react-app/src/App.js
@@ -1,21 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  
+    const [data, setData] = useState([]);
+    
+  
+    // const fetchData = () => {
+    //   return fetch( "calendar?id=1")
+    //       .then((response) => response.json())
+    //       .then((data) => setData(data));
+  
+    // }
+
+    useEffect(() => {
+      
+
+    fetch('http://localhost:8080/calendar?id=1', )
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+        
+
+      })
+  }, []);
+
+
+  //   console.log(data);
+  //   for(let info in data){
+  //     console.log(info.message)
+  //   }
+
+  //  console.log("dm" + data.message)
+    
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload and hello again.
         </p>
+        <p>{data.message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and Hello
         </a>
       </header>
     </div>
