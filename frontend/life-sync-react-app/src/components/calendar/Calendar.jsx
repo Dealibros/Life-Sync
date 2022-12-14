3d7b0766fbaaf319cea02283368d92eb6099bc36
Dealibros <dealibros88@gmail.com>
2022-12-14 17:55:22 +0100

    pulling

---
+++ frontend/life-sync-react-app/src/components/calendar/Calendar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import { useHistory } from "react-router-dom";
+import { format } from "date-fns";
+
+const MyCalendar = ({ updateCurrentMonth }) => {
+    /**Calendar state and functions */
+    const [value, setValue] = useState(new Date());
+  
+    function onChange(nextValue) {
+      setValue(nextValue);
+    }
+  
+    /** OnClick function to go to daily view*/
+    let history = useHistory();
+  
+    const handleClickDate = (value) => {
+      let formattedDate = format(value, "y-MM-dd");
+      history.push(`/date/${formattedDate}`);
+    };
+  
+    const tileContent = ({ date, view }) =>
+
+      view === "month" && date.getDay() === 2 ? <p></p> : null;
+
+      return (
+        <div className="wrapper">
+          <Calendar
+            tileContent={tileContent}
+            onChange={onChange}
+            defaultView="month"
+            value={value}
+            prev2Label={null}
+            next2Label={null}
+            onClickDay={(value, event) => handleClickDate(value, event)}
+            onActiveStartDateChange={({ activeStartDate, value, view }) => {
+              updateCurrentMonth(activeStartDate.getMonth());
+            }}
+          />
+      </div>
+      );
+    };
