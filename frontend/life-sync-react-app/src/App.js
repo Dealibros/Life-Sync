49925e5f879a904828dcd0250d3c07fd8764cad2
dorianamaria96 <dorianamaria96@gmail.com>
2022-12-14 16:46:05 +0100

    restructured frontend folder and small cleanup

--- frontend/life-sync-react-app/src/App.js
+++ frontend/life-sync-react-app/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CalendarPage from './pages/CalendarPage';
-import reportWebVitals from './reportWebVitals';
 
-//const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 export default function App() {
@@ -21,61 +18,3 @@ export default function App() {
   );
 }
 
-//root.render(<App />);
-reportWebVitals();
-
-
-
-
-// import logo from './media/life_sync_logo.png';
-// import './App.css';
-// import React, { useEffect, useState } from "react";
-
-
-// function App() {
-  
-//     const [data, setData] = useState([]);
-    
-  
-//     // const fetchData = () => {
-//     //   return fetch( "calendar?id=1")
-//     //       .then((response) => response.json())
-//     //       .then((data) => setData(data));
-  
-//     // }
-
-//     useEffect(() => {
-      
-
-//     fetch('http://localhost:8080/calendar?id=1', )
-//       .then(response => response.json())
-//       .then(data => {
-//         setData(data);
-        
-
-//       })
-//   }, []);
-
-// console.log(data);
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p className="welcome-message">
-//           Welcome to <span className="life-sync-title">Life Sync</span></p>
-//         <p>{data.message}</p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Let's get started!
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
