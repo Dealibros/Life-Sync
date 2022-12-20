36b865ab54e42c1095f190547824f17b55e40c4e
Dealibros <dealibros88@gmail.com>
2022-12-20 22:06:49 +0100

    working on month change

--- frontend/life-sync-react-app/src/pages/CalendarPage.jsx
+++ frontend/life-sync-react-app/src/pages/CalendarPage.jsx
@@ -1,21 +1,18 @@
 
-import React, { useEffect, useState } from "react";
-import { format } from "date-fns";
+import React, {useState} from "react";
+import { format, set } from "date-fns";
 import EventsBoard from "../components/eventsBoard/EventsBoard"
 import Calendar from 'react-calendar'; 
 import 'react-calendar/dist/Calendar.css';
 import '../styles/CalendarPage.css';
 import '../styles/CalendarReact.css';
-
-
-
+import { daysToWeeks } from "date-fns/esm";
 
 
 
 export default function CalendarPage() {
     const today = new Date();
     const [date, setDate] = useState(new Date());
-    console.log(date)
 
   let greeting = "";
   if (today.getHours() < 12) {
@@ -26,16 +23,28 @@ export default function CalendarPage() {
     greeting = "Good evening!";
   }
 
-  let month = "march"
 
-  console.log(date.toLocaleString('en-US', {month:'long'}))
+  // function previousMonth(){
+  //   setDate(`${(date.getMonth()+1)%12 + 1}, ${date.getDay()} ${date.getFullYear()}`.toLocaleString('en-US', {weekday:'long', day:'numeric', month: 'long'}));
+  // }
+
+  // function nextMonth(){
+  //   setDate(`${(date.getMonth()+1)%12 + 1}, ${date.getDay()} ${date.getFullYear()}`.toLocaleString('en-US', {weekday:'long', day:'numeric', month: 'long'}));
+  // }
+
+
+  // Returns the month after and before
+  const nextMonth = `${(date.getMonth()+1)%12 + 1}, ${date.getDay()} ${date.getFullYear()}`.toLocaleString('en-US', {weekday:'long', day:'numeric', month: 'long'});
+  const previousMonth = `${(date.getMonth()+11)%12 + 1}, ${date.getDay()} ${date.getFullYear()};`.toLocaleString('en-US', {weekday:'long', day:'numeric', month: 'long'})
+
+  console.log("plus " + nextMonth)
+  console.log("minus " + previousMonth)
 
   return (
     <body style={{ 
     backgroundImage: `url(/backgrounds/watercolor-${date.toLocaleString('en-US', {month:'long'})}.jpg` ,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat' }}>
-
 
     <div className="App">
      <div className="wrapper">
@@ -51,16 +60,205 @@ export default function CalendarPage() {
      <h2 className="section-title">Planner</h2>
      <div className="action-area">
       <div className="centerCalendar">
-     
-      <Calendar onChange={setDate} value={date} defaultView='month' maxDetail='month' 
-      nextLabel='>>' 
-      nextAriaLabel='Go to next month'
-      next2Label='>'
-      next2AriaLabel='Go to next year'
-      prevLabel='<<'
-      prevAriaLabel='Go to prev month'
-      prev2Label='<'
-      prev2AriaLabel='Go to prev year'/>
+
+      <Calendar  
+      value={date} 
+      onChange={setDate} 
+      showNeighboringMonth="false"
+      // onActiveStartDateChange={onActiveStartDateChange}
+      // 
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      // prevLabel = {previousMonth}
+      // nextLabel = {nextMonth}
+      />
+
+
       <EventsBoard/>
       </div>
       </div>
